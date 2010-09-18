3af1a1978a1cf7ec8c0003ae648c1bd35059f473
Sven Riedel <sr@gimp.org>
2010-09-18 16:13:25 +0200

    Placeholders for other search methods

--- lib/bri/search/method.rb
+++ lib/bri/search/method.rb
@@ -10,13 +10,24 @@ module Bri
 
       def search( type = :fully_qualified )
         case type
-          when :fully_qualified then fully_qualified_search
+          when :fully_qualified     then fully_qualified_search
+          when :partially_qualified then partially_qualified_search
+          when :unqualified         then unqualified_search
+          else :error # TODO: Error handling
         end
       end
 
       private
       def fully_qualified_search
         @matches << Bri::Match::Method.new( method_rdoc( store_for_method ) )
+      end
+
+      def partially_qualified_search  
+        raise "Implement me!"
+      end
+
+      def unqualified_search
+        raise "Implement me!"
       end
     end
   end
