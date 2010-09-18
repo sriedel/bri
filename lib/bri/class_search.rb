27e42bd670fec5e22b175a05d05d49e073ea9dc5
Sven Riedel <sr@gimp.org>
2010-09-18 12:02:12 +0200

    Add search to Bri::ClassSearch

--- lib/bri/class_search.rb
+++ lib/bri/class_search.rb
@@ -7,8 +7,11 @@ module Bri
       @matches = []
     end
 
-    def search
-      
+    def search( type = :fully_qualified )
+      # NOTE: classes are only searched as fully qualified for the time being
+      # FIXME: What do we do if more than one store defines the same class?
+      store = Bri::Mall.instance.stores.detect { |s| s.modules.include? @term }
+      @matches << Bri::ClassMatch.new( store.load_class( @term ) )
     end
 
   end
