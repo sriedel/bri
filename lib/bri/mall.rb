459cb42fc6ad0f70f7e2cb9b975374da2272eed5
Sven Riedel <sr@gimp.org>
2010-09-18 12:01:08 +0200

    Make Bri::Mall a singleton

--- lib/bri/mall.rb
+++ lib/bri/mall.rb
@@ -1,18 +1,12 @@
 require 'rdoc/ri/paths'
 require 'rdoc/ri/store'
+require 'singleton'
 
 module Bri
   class Mall
+    include Singleton
+
     attr_reader :stores
-
-    def initialize
-      @stores = []
-
-      # We want: system, site, home and gem documentation
-      RDoc::RI::Paths.each( true, true, true, true ) do |path, type|
-        @stores << RDoc::RI::Store.new( path, type ).tap { |store| store.load_cache }
-      end
-    end
 
     def classes
       @stores.collect { |store| store.modules }.flatten.uniq.sort
@@ -33,5 +27,16 @@ module Bri
       end
       methods
     end
+
+    private
+    def initialize
+      @stores = []
+
+      # We want: system, site, home and gem documentation
+      RDoc::RI::Paths.each( true, true, true, true ) do |path, type|
+        @stores << RDoc::RI::Store.new( path, type ).tap { |store| store.load_cache }
+      end
+    end
+
   end
 end
