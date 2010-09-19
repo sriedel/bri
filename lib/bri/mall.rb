6e0d58407c75be45a521a8cff562e859481f4903
Sven Riedel <sr@gimp.org>
2010-09-19 16:30:10 +0200

    Remove unused methods

--- lib/bri/mall.rb
+++ lib/bri/mall.rb
@@ -12,22 +12,6 @@ module Bri
       @stores.collect { |store| store.modules }.flatten.uniq.sort
     end
 
-    def class_methods_by_class
-      methods = {}
-      @stores.each do |store|
-        methods.merge!( store.class_methods ) { |key, m1, m2| m1.concat(m2) }
-      end
-      methods
-    end
-
-    def instance_methods_by_class
-      methods = {}
-      @stores.each do |store|
-        methods.merge!( store.instance_methods ) { |key, m1, m2| m1.concat(m2) }
-      end
-      methods
-    end
-
     private
     def initialize
       @stores = []
