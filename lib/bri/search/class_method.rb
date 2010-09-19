34fcbe285d242c69021da1a33aa26336f4913a48
Sven Riedel <sr@gimp.org>
2010-09-19 09:25:37 +0200

    Consolidate per method store methods

--- lib/bri/search/class_method.rb
+++ lib/bri/search/class_method.rb
@@ -2,22 +2,24 @@ module Bri
   module Search
     class ClassMethod < Method
       private
+      def store_methods( store )
+        store.class_methods
+      end
+
       def store_for_method
         Bri::Mall.instance.stores.detect do |store|
-          store.class_methods.has_key?( @class_term ) &&
-          store.class_methods[@class_term].include?( @method_term )
+          store_methods( store ).has_key?( @class_term ) &&
+          store_methods( store )[@class_term].include?( @method_term )
         end
       end
 
       def classes_with_method( store, method )
-        store.class_methods.
-              select { |klass, methods| methods.include? method }.
-              keys
+        store_methods( store ).select { |klass, methods| methods.include? method }.keys
       end
 
       def candidates_from_method_re( store, method_re )
         candidates = {}
-        store.class_methods.each do |klass, methods|
+        store_methods( store ).each do |klass, methods|
           matching_methods = methods.grep( method_re )
           next if matching_methods.empty?
           candidates[klass] = matching_methods
--- lib/bri/search/instance_method.rb
+++ lib/bri/search/instance_method.rb
@@ -2,22 +2,24 @@ module Bri
   module Search
     class InstanceMethod < Method
       private
+      def store_methods( store )
+        store.instance_methods
+      end
+
       def store_for_method
         Bri::Mall.instance.stores.detect do |store|
-          store.instance_methods.has_key?( @class_term ) &&
-          store.instance_methods[@class_term].include?( @method_term )
+          store_methods( store ).has_key?( @class_term ) &&
+          store_methods( store )[@class_term].include?( @method_term )
         end
       end
 
       def classes_with_method( store, method )
-        store.instance_methods.
-              select { |klass, methods| methods.include? method }.
-              keys
+        store_methods( store ).select { |klass, methods| methods.include? method }.keys
       end
 
       def candidates_from_method_re( store, method_re )
         candidates = {}
-        store.instance_methods.each do |klass, methods|
+        store_methods( store ).each do |klass, methods|
           matching_methods = methods.grep( method_re )
           next if matching_methods.empty?
           candidates[klass] = matching_methods
