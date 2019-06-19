ee306e1db30ee453b3f7774ba29279931f0d424f
Sven Riedel <sr@gimp.org>
2019-06-19 19:14:12 +0200

    Introduce --all and prefer core docs by default

--- lib/bri.rb
+++ lib/bri.rb
@@ -11,7 +11,7 @@ require_relative 'bri/match'
 module Bri
   DEFAULT_WIDTH = 72
 
-  def self.ri( query )
+  def self.ri( query, show_all: false )
     results = Bri::Matcher.new( query ).find
 
     if results.size == 0
@@ -19,7 +19,14 @@ module Bri
     elsif results.size == 1
       results.first.to_s
     elsif results.all? { |r| r.is_a?(Bri::Match::Class) }
-      results.map(&:to_s)
+      if show_all
+        results.map(&:to_s)
+      else
+        gem_docs, core_docs = results.partition { |r| r.origin =~ %r{gem\b} }
+
+        docs_to_output = core_docs.empty? ? gem_docs : core_docs
+        docs_to_output.map(&:to_s)
+      end
     else
       qualified_methods = results.map(&:full_name).sort
       ERB.new( Bri::Templates::MULTIPLE_CHOICES, nil, '<>' ).result( binding )
