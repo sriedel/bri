91d49753c331ebfccbe08f75fd721abecdfdb837
Sven Riedel <sr@gimp.org>
2019-06-24 17:33:23 +0200

    Remove extra newline after header

--- lib/bri/renderer/heading.rb
+++ lib/bri/renderer/heading.rb
@@ -2,7 +2,7 @@ module Bri
   module Renderer
     class Heading < Default
       def extract_text( width, conserve_newlines = false )
-        "<h>#{element.text}</h>\n" 
+        "<h>#{element.text.chomp}</h>" 
       end
     end
   end
