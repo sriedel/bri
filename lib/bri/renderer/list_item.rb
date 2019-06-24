b380a3dc0b38adecd34b67f88f8a8c8937fdf46d
Sven Riedel <sr@gimp.org>
2019-06-24 17:51:40 +0200

    Be a bit more GC friendly when rendering list items

--- lib/bri/renderer/list_item.rb
+++ lib/bri/renderer/list_item.rb
@@ -4,13 +4,17 @@ module Bri
       def text( total_width, bullet )
         element.parts.map.with_index do |part, index|
           part_is_list = part.is_a?(RDoc::Markup::List)
+          first_line_indent = ( part_is_list || index > 0 ) ? bullet.size : 0
+          following_line_indent = bullet.size
 
           text = ::Bri::Renderer.new( part ).extract_text( total_width, true )
           text.prepend( bullet ) if index == 0 && !part_is_list
           lines = text.lines
-          first_line_indent = ( part_is_list || index > 0 ) ? bullet.size : 0
-          following_line_indent = bullet.size
-          [ indent( lines.first, indent_depth: first_line_indent ) ].concat( lines.drop( 1 ).map { |line| ( indent( line, indent_depth: following_line_indent ) ) } ).join
+
+          result = indent( lines.first, indent_depth: first_line_indent )
+          lines.drop( 1 ).each { |line| result << indent( line, indent_depth: following_line_indent ) }
+
+          result
         end
       end
     end
